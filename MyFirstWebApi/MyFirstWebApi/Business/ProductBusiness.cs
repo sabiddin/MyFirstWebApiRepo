9440b2057d701c2845a25bcc5a8dd509b051c741
sabiddin <abiddinsyed@gmail.com>
2018-12-09 00:20:00 -0500

    Added Code.

---
+++ MyFirstWebApi/MyFirstWebApi/Business/ProductBusiness.cs
@@ -0,0 +1,43 @@
+﻿using MyFirstWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstWebApi.Business
+{
+    public class ProductBusiness
+    {
+        private List<Product> _Products = new List<Product>() {
+            new Product {ProductId =1,ProductName ="Product 1", Price = 10.00 },
+                 new Product {ProductId =2,ProductName ="Product 2", Price = 10.00 } ,
+                      new Product {ProductId =3,ProductName ="Product 3", Price = 10.00 }         
+        };
+        public List<Product> GetProducts() {
+            return _Products;
+        }
+        public Product GetProduct(int id) {
+
+            return _Products.FirstOrDefault(p => p.ProductId == id);
+
+            //Product product = new Product();
+            //foreach (var prod in _Products)
+            //{
+            //    if (prod.ProductId ==id)
+            //    {
+            //        product = prod;
+            //    }
+            //}
+            //return product;
+        }
+        public bool Create(Product product)
+        {
+           int maxId = _Products.Max(p => p.ProductId);
+            product.ProductId = maxId++;
+
+            _Products.Add(product);
+            return true;
+        }
+
+    }
+}
---
+++ MyFirstWebApi/MyFirstWebApi/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using MyFirstWebApi.Business;
+using MyFirstWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyFirstWebApi.Controllers
+{
+    [RoutePrefix("api/Product")]
+    public class ProductController : ApiController
+    {
+        private ProductBusiness _ProductBusiness = new ProductBusiness();
+        [Route("GetProducts")]
+        public IHttpActionResult GetProducts() {
+            var products = _ProductBusiness.GetProducts();
+            return Ok(products);
+        }
+        [Route("GetProduct/{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest("Product ID must be non zero number!");
+            }
+            var product = _ProductBusiness.GetProduct(id);
+            return Ok(product);
+        }
+        [Route("Create")]
+        public IHttpActionResult Create(Product product)
+        {
+            if (product == null)
+                return BadRequest();
+            return Ok(_ProductBusiness.Create(product));
+        }
+    }
+}
